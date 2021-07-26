7105823cacebb52a5a5648d02755e4dfc06eba06
PovelikinRostislav <povelikin.rostislav@gmail.com>
2021-07-26 23:51:05 +0300

    Tried different unittest APIs

--- unittest/main.py
+++ unittest/main.py
@@ -1,11 +1,26 @@
 import unittest
 
-def foo():
+def return_42():
     return 42
 
-class TestFoo(unittest.TestCase):
-    def test_return_value(self):
-        self.assertEqual(foo(), 42)
+def raise_exception():
+    raise Exception("This is exception!")
+
+class TestUnittest(unittest.TestCase):
+    def test_assert_equal(self):
+        self.assertEqual(return_42(), 42)
+
+    def test_assert_true(self):
+        boolean_list = [False, False, True]
+        self.assertTrue(any(boolean_list))
+
+    def test_assert_false(self):
+        boolean_list = [True, True, True, False]
+        self.assertFalse(all(boolean_list))
+
+    def test_assert_raises(self):
+        with self.assertRaises(Exception):
+            raise_exception()
 
 if __name__ == '__main__':
     unittest.main()
