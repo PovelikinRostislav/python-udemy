88eb8c9ca5b23ad882cc4917a9d773940caf181b
PovelikinRostislav <povelikin.rostislav@gmail.com>
2021-07-27 18:35:40 +0300

    Added explanation for namespace pkgs and rationale behind it

--- packages_and_modules/namespace_package.py
+++ packages_and_modules/namespace_package.py
@@ -10,3 +10,8 @@ print("it's __path__ contains both of those paths cause it's a namespace package
 print("Also we can see this in submodule_search_locations of module's spec:")
 print(f"__path__ of parent package: {parent.__path__}")
 print(f"__spec__ of parent package: {parent.__spec__}")
+
+print("----")
+print("Namespace packages are useful ONLY for the cases when different libraries contibute to the same package")
+print("Otherwise, use __init__.py files to indicate regular packages. If you are not following this, many built-in tools will fail")
+print("E.g. unittest won't search subdirs w/o __init__.py cause it's not regular package")
