282cbf0baebe9c67fff5ad51641b9ed0067a62d1
PovelikinRostislav <povelikin.rostislav@gmail.com>
2021-08-02 20:55:40 +0300

    Added c-tags temp file to gitignore

--- war_card_game/tests/test_game.py
+++ war_card_game/tests/test_game.py
@@ -10,7 +10,16 @@ def prepare_game_decks():
 class TestGame(unittest.TestCase):
     def test_game_logic(self):
         deck_one, deck_two = prepare_game_decks()
+
         self.assertEqual(len(deck_one), (52//2))
         self.assertEqual(len(deck_two), len(deck_one))
+        self.assertTrue(deck_one.shuffled)
+        self.assertTrue(deck_two.shuffled)
 
-        self.assertTrue(True)
+        finished_game = False
+
+        while not finished_game:
+            finished_game = True
+
+        # Finish
+        self.assertTrue(finished_game)
