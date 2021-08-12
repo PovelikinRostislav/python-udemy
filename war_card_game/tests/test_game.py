7d74d40026960b278ae0b6f730dcb670d3704b2a
PovelikinRostislav <povelikin.rostislav@gmail.com>
2021-08-12 20:28:07 +0300

    Added logic of appending cards if the cards on the table are not equal

--- war_card_game/tests/test_game.py
+++ war_card_game/tests/test_game.py
@@ -11,16 +11,20 @@ class TestGame(unittest.TestCase):
     def test_game_logic(self):
         deck_one, deck_two = prepare_game_decks()
 
-        self.assertEqual(len(deck_one), (52//2))
-        self.assertEqual(len(deck_two), len(deck_one))
-        self.assertTrue(deck_one.shuffled)
-        self.assertTrue(deck_two.shuffled)
-
         finished_game = False
 
         while not finished_game and len(deck_one) != 0 and len(deck_two) != 0:
             deck_one_card = deck_one.get_card()
             deck_two_card = deck_two.get_card()
+
+            if deck_one_card < deck_two_card:
+                deck_one.append_card(deck_one_card)
+                deck_one.append_card(deck_two_card)
+            elif deck_one_card > deck_two_card:
+                deck_two.append_card(deck_one_card)
+                deck_two.append_card(deck_two_card)
+            else:
+                pass
 
             finished_game = True
 
