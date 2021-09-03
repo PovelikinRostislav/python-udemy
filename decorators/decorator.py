944b5185388be1dac803f7104ace10f287bda73c
PovelikinRostislav <povelikin.rostislav@gmail.com>
2021-09-03 18:36:50 +0300

    Added decorator example

--- decorators/decorator.py
+++ decorators/decorator.py
@@ -8,11 +8,21 @@ def wrap(func):
         print("That's the new function msg after func was executed")
     return new_function
 
+@wrap
+def func_to_decorate(arg):
+    print(f"That's the function to decorate with an arg {arg}")
+
 def main():
     func_to_wrap(5)
     wrapped_function = wrap(func_to_wrap)
     wrapped_function()
+    # Next call shows the main diff between decorator and decorator-like impl
+    # Decorator-like implementation doesn't substitute the previous func definition
+    # (unless you rebind the function name to the same one)
     func_to_wrap(13)
+
+    print("=====")
+    func_to_decorate()
 
 if __name__ == "__main__":
     main()
